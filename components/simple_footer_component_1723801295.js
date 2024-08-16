e97ea6aaf1f63ecbf400270edc209723d88f10b7
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-16 09:56:23 +0000

    Merge code from frontend

--- components/simple_footer_component_1723801295.js
+++ components/simple_footer_component_1723801295.js
@@ -2,13 +2,13 @@
 */
 Vue.component("simple_footer_component_1723801295", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg" style="min-height: 390px;">
+    <footer id="footer-section" style="min-height: 670px" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg" >
         <div id="footer-container" class="max-w-screen-xl p-8 py-12 mx-auto lg:py-16 md:p-12 lg:p-16">
             <hr id="footer-divider" class="my-8 border-white border-opacity-20 sm:mx-auto lg:my-12">
             <div id="footer-content" class="text-center">
                 <div class="flex">
                     <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-6 text-3xl font-bold text-white hover:text-pink-200 transition-colors duration-300">
-                        <img id="footer-logo" src="./images/logo.svg" alt="Landwind Logo" class="h-8 mr-4 sm:h-10">
+                        <img id="footer-logo" src="https://makeinfinite-mentat-test.azurewebsites.net/get_image/Hen_Care_Companion_1723801294/logo.svg" alt="Landwind Logo" class="h-8 mr-4 sm:h-10">
                         ChickenChatter's
                     </a>
                 </div>
